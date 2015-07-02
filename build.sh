34b40540b24935c4f6d169581b860c39825711f4
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-07-02 18:52:29 +0100

    also build ro bundle

--- build.sh
+++ build.sh
@@ -6,5 +6,15 @@ sha1sum data/* > manifest-sha1.txt
 rm data/external.txt # :)
 cd ..
 rm -f example1.zip
-zip -r example1.zip example1
+zip -q -r example1.zip example1
 echo "Built BagIt example1.zip"
+
+
+rm -f example1.bundle.zip
+cd example1
+echo -n application/vnd.wf4ever.robundle+zip > mimetype
+zip -q -0 -X ../example1.bundle.zip mimetype
+rm mimetype
+zip -q -r ../example1.bundle.zip .
+echo "Built RO Bundle example1.bundle.zip"
+cd ..
