5cd66dc2d96192e9c19207e41e0599c94aa31dd9
Stian Soiland-Reyes <soiland-reyes@manchester.ac.uk>
2023-01-13 12:41:05 +0000

    no more sha256

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 cd example1
 curl https://gist.githubusercontent.com/anonymous/7fe620279ea4988a5a1e/raw/e55d9ea6af35ea67cfaf47b03a2b71f9026325fd/external.txt > data/external.txt
 
-sha512sum data/* > manifest-sha512.txt
+sha512sum data/*.* data/provenance/* > manifest-sha512.txt
 rm -f data/external.txt # :)
 
 sha512sum manifest* fetch.txt bag*txt > tagmanifest-sha512.txt
